6ea2ebb254e85a14a76e9adfdc3617ad8401b685
Sagor31h2 <sagor31h2@gmail.com>
2021-06-14 18:27:19 +0600

    Add Member Class

---
+++ Member form-.net5_with_angular11/Models/Member.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Member_form_.net5_with_angular11.Models
+{
+    public class Member
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Column(TypeName="nvarchar=(50)")]
+        public string FirstName { get; set; }
+
+        [Column(TypeName = "nvarchar=(50)")]
+        public string MiddleName { get; set; }
+
+        [Column(TypeName = "nvarchar=(50)")]
+        public string LastName { get; set; }
+    }
+}
