ceef563961d644cbb6b8ab47aacce1ff432a29dc
Sagor31h2 <sagor31h2@gmail.com>
2021-06-15 18:15:29 +0600

    Add DbContext class

---
+++ Member form-.net5_with_angular11/Models/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Member_form_.net5_with_angular11.Models
+{
+    public class AppDbContext
+    {
+        private readonly DbContextOptions _options;
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+            _options = options;
+        }
+
+        public DbSet<Member> members { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
