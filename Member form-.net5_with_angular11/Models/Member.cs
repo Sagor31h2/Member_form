3275d300fcbec042f83b9151ca77dc86f2196128
Sagor31h2 <sagor31h2@gmail.com>
2021-07-15 13:55:00 +0600

    Add Migration

--- Member form-.net5_with_angular11/Models/Member.cs
+++ Member form-.net5_with_angular11/Models/Member.cs
@@ -11,14 +11,13 @@ namespace Member_form_.net5_with_angular11.Models
     {
         [Key]
         public int Id { get; set; }
-
-        [Column(TypeName="nvarchar=(50)")]
+        [Column(TypeName = "nvarchar(50)")]
         public string FirstName { get; set; }
 
-        [Column(TypeName = "nvarchar=(50)")]
-        public string MiddleName { get; set; }
-
-        [Column(TypeName = "nvarchar=(50)")]
+        [Column(TypeName = "nvarchar(50)")]
         public string LastName { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string MiddleName { get; set; }
     }
 }
