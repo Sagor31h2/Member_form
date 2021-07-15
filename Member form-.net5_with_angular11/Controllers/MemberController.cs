b1a1d1cfc01e1f89951bc96e33fc53f8238e2bde
Sagor31h2 <sagor31h2@gmail.com>
2021-07-15 14:07:21 +0600

    Add controller

---
+++ Member form-.net5_with_angular11/Controllers/MemberController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Member_form_.net5_with_angular11.Models;
+
+namespace Member_form_.net5_with_angular11.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MemberController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public MemberController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Member
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Member>>> Getmembers()
+        {
+            return await _context.members.ToListAsync();
+        }
+
+        // GET: api/Member/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Member>> GetMember(int id)
+        {
+            var member = await _context.members.FindAsync(id);
+
+            if (member == null)
+            {
+                return NotFound();
+            }
+
+            return member;
+        }
+
+        // PUT: api/Member/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMember(int id, Member member)
+        {
+            if (id != member.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(member).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MemberExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Member
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Member>> PostMember(Member member)
+        {
+            _context.members.Add(member);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetMember", new { id = member.Id }, member);
+        }
+
+        // DELETE: api/Member/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMember(int id)
+        {
+            var member = await _context.members.FindAsync(id);
+            if (member == null)
+            {
+                return NotFound();
+            }
+
+            _context.members.Remove(member);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool MemberExists(int id)
+        {
+            return _context.members.Any(e => e.Id == id);
+        }
+    }
+}
