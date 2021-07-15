a55974002e85ef09d1280e63cefce4b54ebf9f19
Sagor31h2 <sagor31h2@gmail.com>
2021-07-15 14:27:44 +0600

    Add class and membe and sub member from

--- Member form-.net5_with_angular11/ClientApp/src/app/app.module.ts
+++ Member form-.net5_with_angular11/ClientApp/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MemberComponent } from './member/member.component';
+import { MemberFormComponent } from './member/member-form/member-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MemberComponent,
+    MemberFormComponent
   ],
   imports: [
     BrowserModule
---
+++ Member form-.net5_with_angular11/ClientApp/src/app/member/member-form/member-form.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-member-form',
+  templateUrl: './member-form.component.html',
+  styles: [
+  ]
+})
+export class MemberFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
